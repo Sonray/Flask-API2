4d8fe2360616fbf0ae5edbbfd334bda5b6c3d7aa
David Okwacha <osweredavid@gmail.com>
2020-09-14 06:59:28 +0300

    initialised the app by creating the create app function

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,28 @@
 from flask import Flask
-from .config import DevConfig
+from flask_bootstrap import Bootstrap
+from config import config_options
 
-# Initializing application
-app = Flask(__name__)
+# Initialize flask extensions
+bootstrap = Bootstrap()
 
-# Setting up configuration
-app.config.from_object(DevConfig)
+def create_app(config_name):
+	# initialize application
+	app = Flask(__name__)
 
-from app import views
+	# set up configurations
+	app.config.from_object(config_options[config_name])
+	app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+	# initialize flask extensions
+	bootstrap.init_app(app)
+
+	# registering the blueprint
+	from .main import main as main_blueprint
+
+	app.register_blueprint(main_blueprint)
+
+	# setting config 
+	from .request import configure_request
+	configure_request(app)
+
+	return app 
