1d1cae44e1e8368d5611f00ecf26e956e27c766e
David Okwacha <osweredavid@gmail.com>
2020-09-14 06:55:45 +0300

    moved views file to main folder

--- app/news.py
+++
@@ -1,8 +0,0 @@
-class news:
-    def __init__(self, news_id, name, author, title, description, news_date):
-        self.news_id = news_id
-        self.name = name
-        self.author = author
-        self.title = title
-        self.description = description
-        self.news_date = news_date
