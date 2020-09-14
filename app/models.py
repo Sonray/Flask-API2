e0bab0b85c5e0dc3e70b4a7f74bd1c8bfb759e6e
David Okwacha <osweredavid@gmail.com>
2020-09-14 07:09:51 +0300

    created class source model

---
+++ app/models.py
@@ -0,0 +1,13 @@
+class Source:
+	'''
+	Source Class to define Source Objects
+	'''
+	def __init__(self,id,name,description,url,category):
+		'''
+		Function to initialize Source Objects
+		'''
+		self.id = id
+		self.name = name
+		self.description = description
+		self.url = url
+		self.category = category
