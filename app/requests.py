f21b85601c7abd206b9d3e513f1fbf605adc64aa
David Okwacha <osweredavid@gmail.com>
2020-09-14 07:17:44 +0300

    created function to process results

--- app/requests.py
+++ app/requests.py
@@ -32,3 +32,20 @@ def get_sources(category):
 			sources_results = process_sources(sources_results_list)
 
 	return sources_results
+    
+def process_sources(sources_results):
+	'''
+	Function  that processes the sources result and transform them to a list of Objects
+	'''
+	sources_list = []
+	for source_item in sources_results:
+		id = source_item.get('id')
+		name = source_item.get('name')
+		description = source_item.get('description')
+		url = source_item.get('url')
+		category = source_item.get('category')
+
+		source_object = Source(id,name,description,url,category)
+		sources_list.append(source_object)
+
+	return sources_list
